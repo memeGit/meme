5c9ba0d5dd803da40cb2dfc1148ce08d1f65b0eb
Drealdal <meiqizhu@gmail.com>
2012-03-15 18:04:33 +0800

    files

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Created on 2012-3-15
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+<div class="section-2">
+<div class="wrap">
+
+<ul class="info footer-left">
+	<li>9GAG &copy; 2011</li>
+	<li>·<a class="badge-language-selector" href="javascript:void(0);">&#31777;&#39636;&#20013;&#25991; ( zh_CN )</a></li>
+</ul><!--end ul.footer-left-->
+
+<ul class="info footer-right">
+	<li><a href="about">关于</a></li>
+	<li>·<a href="rules">9 條規則</a></li>
+	<li>·<a href="faq">常见问题</a></li>
+	<li>·<a href="tos">条款</a></li>
+	<li>·<a href="privacy">私隐</a></li>
+	<li>·<a href="contact">联络</a></li>
+</ul><!--end ul.footer-right-->
+
+</div>
+</div>
